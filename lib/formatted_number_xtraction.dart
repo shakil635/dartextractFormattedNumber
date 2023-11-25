dc569f4a23f5c240c5873ba92e3c6c8888994ff1
unknown <shakiladnann635@gmail.com>
2023-11-26 00:33:14 +0600

    add new line againg

--- lib/formatted_number_xtraction.dart
+++ lib/formatted_number_xtraction.dart
@@ -7,15 +7,11 @@ export 'src/formatted_number_xtraction_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
 
-String extractFormattedNumber(String numbers) {
-  final stringSplit = numbers.split(" ");
-  final string1 = stringSplit[0];
-  final repless = string1.replaceAll("(", "+").replaceAll(")", "-");
+String? extractFormattedNumber(String phoeNumber) {
+  if (!phoeNumber.startsWith("()")) {
+    return null;
+  }
+  final reples = phoeNumber.replaceAll("(", "+").replaceAll(")", "");
 
-  return repless;
-}
-
-void main() {
-  String ab = '(123) 456-7890';
-  print(extractFormattedNumber(ab));
+  return reples;
 }
